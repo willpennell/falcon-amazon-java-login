16540193cc4417a33e6817622cb79ddeed3c4f86
William Pennell <william.pennell@jumar.co.uk>
2023-06-01 17:59:29 +0100

    Changed Strucutre so root works

--- login/src/main/java/com/teamfalcon/login/exceptions/IncorrectPasswordException.java
+++
@@ -1,8 +0,0 @@
-package com.teamfalcon.login.exceptions;
-
-public class IncorrectPasswordException extends RuntimeException {
-    private static final String INCORRECT_PASSWORD_MESSAGE = "Error 401 Unauthorised: Incorrect Credentials";
-    public IncorrectPasswordException() {
-        super(INCORRECT_PASSWORD_MESSAGE);
-    }
-}
